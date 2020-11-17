0fc6b589c81c346cfd2261fbaa6f39b3d69dd008
Yuri Oliveira <yuri.pcesar@gmail.com>
2020-11-16 22:16:23 -0300

    ADD

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(9000);
+app.listen(9000, () => {
+  console.log('Rodando na porta 9000')
+});
